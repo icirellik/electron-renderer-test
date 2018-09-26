3cafb1cd535321374370f7861456716e034aab10
Cameron Rollheiser <icirellik@gmail.com>
2018-09-26 11:19:55 -0400

    Fail Command Incorrect
    At some point I changed the failure command making it incorrect.

--- src/index.js
+++ src/index.js
@@ -11,8 +11,3 @@ let child = spawn(electron, args, {
 child.stdout.pipe(process.stdout)
 child.stderr.pipe(process.stderr);
 process.stdin.pipe(child.stdin)
-
-// Listen for ipc call, this should work fine.
-child.on('message', (message) => {
-  console.log(message)
-});
