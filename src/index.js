a9a695945f29aa1aa6dfb7adf9111f9261ed5767
Cameron Rollheiser <icirellik@gmail.com>
2018-09-26 10:09:26 -0400

    Update Notes
    Added some notes to the source.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ const electron = require('electron');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-(async () => {
-  return new Promise(() => {
-    const args = [
-      path.join(__dirname, 'selector.js'),
-    ];
-    let child = spawn(electron, args, {
-      stdio: [ 'pipe', 'pipe', 'pipe', 'ipc' ],
-    });
-    child.stdout.pipe(process.stdout)
-    process.stdin.pipe(child.stdin)
-    child.stderr.on('data', data => {
-      process.stderr.write(data);
-    });
-  });
-})();
+const args = [
+  path.join(__dirname, 'selector.js'),
+];
+let child = spawn(electron, args, {
+  stdio: [ 'pipe', 'pipe', 'pipe', 'ipc' ],
+});
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr);
+process.stdin.pipe(child.stdin)
+
+// Listen for ipc call, this should work fine.
+child.on('message', (message) => {
+  console.log(message)
+});
